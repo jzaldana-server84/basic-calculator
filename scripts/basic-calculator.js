02a0985c0e6b34c0fe87e8963cee021e10be13f8
jzaldana.server84 <jzaldana.server84@gmail.com>
2022-07-19 20:40:36 -0600

    Screen Numbers Functionality

--- scripts/basic-calculator.js
+++ scripts/basic-calculator.js
@@ -6,11 +6,12 @@ const getNumbers = document.querySelectorAll('#number');
 
 // Other Variables
 let onOff = 0;
+let screenText = '';
 
 // Turn ON / OFF Calculator
 const activateCalculator = () => {
     if (onOff == 0) {
-        calcScreen.innerText = 0;
+        calcScreen.innerText = '0';
         onOff = 1;
     } else {
         calcScreen.innerText = '';
@@ -19,6 +20,12 @@ const activateCalculator = () => {
 };
 
 // Alert Numbers
+getNumbers.forEach(function(item){
+    item.addEventListener('click', () => {
+            screenText += item.innerText;
+            calcScreen.innerText = screenText;
+    })
+});
 
 // Events
 // Activate Calculator
